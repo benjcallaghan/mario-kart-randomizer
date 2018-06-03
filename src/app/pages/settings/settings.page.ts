07b172bfbd7b3def67da21f374f864fd12d6b3e8
Ben Callaghan <benj.callaghan@gmail.com>
2018-06-02 23:08:48 -0600

    Added methods to set settings values

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -23,23 +23,43 @@ export class SettingsPage {
 
   get isValidCharacters(): boolean {
     const required = CharacterSize.Small | CharacterSize.Medium | CharacterSize.Large;
-    return this.isCharacterChecked(required);
+    return this.isCharacterEnabled(required);
+  }
+
+  isCharacterEnabled(size: CharacterSize): boolean {
+    return !!(this.settings.allowedCharacters & size);
+  }
+
+  enableCharacter(size: CharacterSize, value: boolean) {
+    if (value) {
+      this.settings.allowedCharacters |= size;
+    } else {
+      this.settings.allowedCharacters &= ~size;
+    }
   }
 
   get isValidVehicles(): boolean {
     const required = VehicleType.Kart | VehicleType.Bike | VehicleType.ATV;
-    return this.isVehicleChecked(required);
+    return this.isVehicleEnabled(required);
   }
 
-  isCharacterChecked(size: CharacterSize): boolean {
-    return !!(this.settings.allowedCharacters & size);
-  }
-
-  isVehicleChecked(vehicle: VehicleType): boolean {
+  isVehicleEnabled(vehicle: VehicleType): boolean {
     return !!(this.settings.allowedVehicles & vehicle);
   }
 
-  isDuplicatesChecked(): boolean {
+  enableVehicle(vehicle: VehicleType, value: boolean) {
+    if (value) {
+      this.settings.allowedVehicles |= vehicle;
+    } else {
+      this.settings.allowedVehicles &= ~vehicle;
+    }
+  }
+
+  isDuplicatesEnabled(): boolean {
     return this.settings.allowDuplicates;
+  }
+
+  enableDuplicates(value: boolean) {
+    this.settings.allowDuplicates = value;
   }
 }
