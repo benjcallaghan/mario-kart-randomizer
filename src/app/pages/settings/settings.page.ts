468568a725d74422aa43dba041e3106e84f95709
Ben Callaghan <benj.callaghan@gmail.com>
2018-06-02 22:34:58 -0600

    Added logic to connect existing settings to the view

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -9,6 +9,10 @@ import { VehicleType } from '../../vehicle-type.enum';
   styleUrls: ['./settings.page.scss'],
 })
 export class SettingsPage {
+  // Needed to access enums inside the template
+  public CharacterSize = CharacterSize;
+  public VehicleType = VehicleType;
+
   private settings: KartSettings = {
     allowedCharacters: CharacterSize.Small | CharacterSize.Medium | CharacterSize.Large | CharacterSize.Mii,
     allowedVehicles: VehicleType.Kart | VehicleType.Bike | VehicleType.ATV,
@@ -19,11 +23,23 @@ export class SettingsPage {
 
   get isValidCharacters(): boolean {
     const required = CharacterSize.Small | CharacterSize.Medium | CharacterSize.Large;
-    return !!(this.settings.allowedCharacters & required);
+    return this.isCharacterChecked(required);
   }
 
   get isValidVehicles(): boolean {
     const required = VehicleType.Kart | VehicleType.Bike | VehicleType.ATV;
-    return !!(this.settings.allowedVehicles & required);
+    return this.isVehicleChecked(required);
+  }
+
+  isCharacterChecked(size: CharacterSize): boolean {
+    return !!(this.settings.allowedCharacters & size);
+  }
+
+  isVehicleChecked(vehicle: VehicleType): boolean {
+    return !!(this.settings.allowedVehicles & vehicle);
+  }
+
+  isDuplicatesChecked(): boolean {
+    return this.settings.allowDuplicates;
   }
 }
