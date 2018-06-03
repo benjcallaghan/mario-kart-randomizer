8e20d565f71299692c413c6f317ad7dd56414826
Ben Callaghan <benj.callaghan@gmail.com>
2018-06-02 21:49:53 -0600

    Added shuffle-all to the toolbar

--- src/app/pages/slots/slots.page.ts
+++ src/app/pages/slots/slots.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, QueryList } from '@angular/core';
 import { MarioService } from '../../services/mario.service';
 import { MkItem } from '../../mk-item';
+import { ContainerComponent } from '../../components/container/container.component';
 
 @Component({
   selector: 'mk-slots',
@@ -17,6 +18,11 @@ export class SlotsPage implements OnInit {
     'Player 1', 'Player 2', 'Player 3', 'Player 4'
   ];
 
+  @ViewChildren('character') private characterSpinners: QueryList<ContainerComponent>;
+  @ViewChildren('vehicle') private vehicleSpinners: QueryList<ContainerComponent>;
+  @ViewChildren('wheel') private wheelSpinners: QueryList<ContainerComponent>;
+  @ViewChildren('glider') private gliderSpinners: QueryList<ContainerComponent>;
+
   constructor(mario: MarioService) {
     this.characters = mario.getAllCharacters();
     this.vehicles = mario.getAllVehicles();
@@ -27,4 +33,61 @@ export class SlotsPage implements OnInit {
   ngOnInit() {
 
   }
+
+  async shuffleItems(): Promise<void> {
+    const shuffleTime = 2000;
+
+    const characters = this.randomizeCharacters();
+    const vehicles = this.randomizeVehicles();
+    const wheels = this.randomizeWheels();
+    const gliders = this.randomizeGliders();
+
+    const promises = flatten(
+      this.characterSpinners.map((spinner, i) => spinner.spin(shuffleTime, characters[i].name)),
+      this.vehicleSpinners.map((spinner, i) => spinner.spin(shuffleTime, vehicles[i].name)),
+      this.wheelSpinners.map((spinner, i) => spinner.spin(shuffleTime, wheels[i].name)),
+      this.gliderSpinners.map((spinner, i) => spinner.spin(shuffleTime, gliders[i].name))
+    );
+    await Promise.all(promises);
+  }
+
+  randomizeCharacters(): MkItem[] {
+    return shuffle(this.characters);
+  }
+
+  randomizeVehicles() {
+    return shuffle(this.vehicles);
+  }
+
+  randomizeWheels() {
+    return shuffle(this.wheels);
+  }
+
+  randomizeGliders() {
+    return shuffle(this.gliders);
+  }
+
+  buildRandomList(list: MkItem[], count: number): MkItem[] {
+    const result: MkItem[] = [];
+
+    for (let i = 0; i < count; i++) {
+      const r = Math.floor(Math.random() * (list.length - 1));
+      result.push(list[r]);
+    }
+
+    return result;
+  }
+}
+
+function flatten<T>(...arr: T[][]): T[] {
+  return arr.reduce((previous, current) => previous.concat(current));
+}
+
+function shuffle<T>(arr: T[]): T[] {
+  const a = arr.slice();
+  for (let i = a.length; i; i--) {
+    const j = Math.floor(Math.random() * i);
+    [a[i - 1], a[j]] = [a[j], a[i - 1]];
+  }
+  return a;
 }
